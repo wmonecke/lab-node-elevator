515f06c218390bd72effb3ff004a030b24317fac
Walter Monecke <wmonecke@gmail.com>
2017-02-20 18:22:26 -0500

    Elevator picks people on the way up! Functionality on the way down to be added

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,72 @@
 class Elevator {
   constructor(){
     this.floor      = 0;
-    this.MAXFLOOR   = 10;
+    this.maxFloor   = 10;
+    this.direction = 'up';
+
     this.requests   = [];
+    this.waitingList = [];
+    this.passengers = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start(person) {
+    var clear = setInterval(() => {
+      this.update();
+      this.floorUp();
+      console.log(this.passengers);
+      this._passengersEnter(person);
+      this._passengersLeave(person);
+    }, 1000);
+   }
+
+  stop() {
+    clearInterval(clear);
+   }
+
+  update() {
+    this.log();
+   }
+
+  _passengersEnter(person) {
+    for (var i = 0; i < this.waitingList.length; i++) {
+      if (this.waitingList[i].originFloor === this.floor) {
+        this.passengers.push(person);
+        this.waitingList.splice(0, 1, person);
+      }
+    }
+  }
+
+  _passengersLeave() {
+    for (var i = 0; i < this.passengers.length; i++) {
+      if (this.passengers[i].destinationFloor === this.floor) {
+        this.passengers.splice(0, 1);
+      }
+    }
+  }
+
+  floorUp() {
+    if(this.floor < this.maxFloor) {
+    this.floor++;
+    }
+  }
+
+  floorDown() {
+    if (this.floor >= 1) {
+      this.floor = this.floor - 1;
+    } else {
+      console.log('You are in floor 0');
+    }
+  }
+
+  call(person) {
+    this.waitingList.push(person);
+    this.requests.push(person.originFloor);
+  }
+
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
+
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,9 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+let myElevator = new Elevator();
+let walt = new Person('Walter', 2, 8);
+myElevator.start(walt);
+myElevator.call(walt);
+myElevator.log();
+console.log(myElevator.waitingList);
